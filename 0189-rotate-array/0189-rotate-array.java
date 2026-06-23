31c65c2f1b6fb57e829274e2d1f0537c6e075668
Muskan Kumari <msingh7763@gmail.com>
2026-06-23 14:50:39 +0530

    Time: 0 ms (100%), Space: 63.2 MB (12.24%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -11,13 +11,33 @@ class Solution {
         // }
 
        
-        for(int i=0;i<n;i++){
-            temp[(i + k) % n] = nums[i];
+        // for(int i=0;i<n;i++){
+        //     temp[(i + k) % n] = nums[i];
+
+        // }
+        // for(int i=0;i<n;i++){
+        //     nums[i]=temp[i];
+        // }
+
+        // using single space
+
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+
+
+        
+
+        
+    }
+    private void reverse(int[] nums,int l,int r){
+        while(l<r){
+            int temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
+            l++;
+            r--;
 
         }
-        for(int i=0;i<n;i++){
-            nums[i]=temp[i];
-        }
-        
     }
 }
